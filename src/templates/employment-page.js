e385aa82b3469fd1a8e06ae9cc34af6b83c43575
edgelord9ooo <avery_tan@hotmail.com>
2020-01-09 01:03:43 -0700

    imported {content} object

--- src/templates/employment-page.js
+++ src/templates/employment-page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Content from '../components/Content'
+import Content, { HTMLContent } from '../components/Content'
 
 export const EmploymentPageTemplate = ({
   title,
